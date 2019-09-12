5146de595ec6e01c9765a3f52d269850334bf224
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-12 08:50:12 -0700

    experimenting with a different approach

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,91 @@
-$("button").on("click", function () {
+// var characters = ["mario", "luigi", "link"];
 
-    // this is the main section that pulls gifs from giphy and from the button div in html
+// function displayGif() {
+//     var character = $(this).attr("data-character");
+
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//             var gifDiv = $("<div class='gif'>");
+
+//             console.log(response);
+
+//             var rating = results[i].rating;
+
+//             $("#gif-view").empty();
+
+
+//             var p = $("<p>").text("Rating: " + rating);
+
+//             gifDiv.append(p);
+
+//             // var gifImage = $("<img");
+
+//             // gifImage.attr("src", results[i].images.fixed_height.url);
+
+//             // gifDiv.prepend(p);
+//             // gifDiv.prepend(gifImage);
+
+//             $("#gif-view").prepend(gifDiv);
+
+//             // for (var i = 0; i < results.length; i++) {
+//             //     var gifDiv = $("<div>");
+//             //     var rating = results[i].rating;
+
+//             //     var p = $("<p>").text("Rating: " + rating);
+
+//             //     var characterImage = $("<img>");
+//             //     characterImage.attr("src", results[i].images.fixed_height.url);
+
+//             //     gifDiv.prepend(p);
+//             //     gifDiv.prepend(characterImage);
+
+//             //     $("#gif-view").prepend(gifDiv);
+//             // }
+
+//         })
+//     function renderButtons() {
+//         $("#button-view").empty();
+//         for (var i = 0; i < characters.length; i++) {
+//             var newButton = $("<button>");
+//             newButton.addClass("gif-btn");
+//             newButton.attr("data-character", characters[i]);
+//             newButton.text(characters[i]);
+//             $("#button-view").append(newButton);
+//         }
+//     }
+
+//     $("#add-gif").on("click", function (event) {
+//         event.preventDefault();
+//         var character = $("#gif-input").val().trim();
+//         characters.push(character);
+//         renderButtons();
+        
+//     });
+
+//     $(document).on("click", ".gif-btn", displayGif);
+//     renderButtons();
+// };
+
+
+
+var characters = [];
+function displayGif() {
+    var gifDiv = $("<div>");
+    $("#gif-view").prepend(gifDiv);
+}
+
+// this is the main section that pulls gifs from giphy and from the button div in html
+// function displayGif() {
+$("button").on("click", function () {
+    // event.preventDefault();
+    // var character = $("#gif-input").val().trim();
+    // characters.push(character);
+    // renderButtons();
+
     var character = $(this).attr("data-character");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
 
@@ -25,17 +110,48 @@ $("button").on("click", function () {
 
                 $("#gif").prepend(gifDiv);
             }
-        });
 
-        // function/s below will focus on pulling new gifs into the existing list and creating new buttons for them
+        })
 
-        function renderButtons(){
-            $("#button-view").empty();
-            for ( var i = 0; i < character.length; i++) {
-                var newButton = $("<button>");
-                newButton.addClass("gif-btn");
-                newButton.attr("data-name", character[i]);
-                newButton.text(character[i]);
-            }
-        }
-});
+
+
+    $(document).on("click", ".gif-btn", displayGif);
+    // renderButtons();
+
+
+//     $(".gif").on("click", function () {
+//         var state = $(this).attr("data-state");
+//         if (state === 'still') {
+//             var animateURL = $(this).attr(queryURL);
+//             $(this).attr("src", animateURL);
+//             $(this).attr("data-state", "animate");
+//         } else {
+//             $(this).attr("src", $(this).attr("data-still"));
+//             $(this).attr("data-state", "still");
+
+//         };
+
+//         function renderButtons() {
+//             $("#button-view").empty();
+//             for (var i = 0; i < characters.length; i++) {
+//                 var newButton = $("<button>");
+//                 newButton.addClass("gif-btn");
+//                 newButton.attr("data-name", characters[i]);
+//                 newButton.text(characters[i]);
+//                 $("#button-view").append(newButton);
+//             }
+//         }
+
+//         $("#add-gif").on("click", function (event) {
+//             event.preventDefault();
+//             var character = $("#gif-input").val().trim();
+
+//             characters.push(character);
+//             // renderButtons();
+
+//             // gifDiv.prepend(p);
+//             // $("#gif").prepend(gifDiv);
+//         });
+//         renderButtons();
+//     })
+})
