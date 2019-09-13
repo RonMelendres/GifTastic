bdf8e050ca995d5caf19127f6c43a994e907ce61
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-12 20:10:27 -0700

    gifs now have the same topic as the corresponding button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
-var characters = ["mario", "link", "luigi", "cat"];
+var characters = ["mario", "link", "luigi", "pikachu"];
 
 function displayGif() {
 
     // $("gif-button").on("click", function() {
 
-    // var character = $(this).attr("data-character");
+    var character = $(this).attr("data-character");
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + characters + "&limit=10&offset=0&rating=PG&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
 
     $.ajax({
         url: queryURL,
@@ -20,6 +20,7 @@ function displayGif() {
             var p = $("<p>").text("Rating: " + rating);
             var gifVideo = $("<img>");
             gifVideo.attr("src", results[i].images.fixed_height.url);
+
 
             // still
             // animate
@@ -41,7 +42,7 @@ function renderButtons() {
     for (var i = 0; i < characters.length; i++) {
         var newButton = $("<button>");
         newButton.addClass("gif-btn");
-        newButton.attr("data-name", characters[i]);
+        newButton.attr("data-character", characters[i]);
         newButton.text(characters[i]);
         $("#button-view").append(newButton);
     }
@@ -88,104 +89,3 @@ $(".gif").on("click", function () {
             // var gif = $("<img>").attr("src", gifURL);
 
             // gifDiv.append(gif);
-
-
-
-// // this is the main section that pulls gifs from giphy and from the button div in html
-// // function displayGif() {
-
-// var characters = ["mario", "link", "luigi", "cat"];
-
-
-// function renderButtons() {
-//     // renderButtons.empty();
-//     $("#button-view").empty();
-//     for (var i = 0; i < characters.length; i++) {
-//         var newButton = $("<button>");
-//         newButton.addClass("gif-btn");
-//         newButton.attr("data-name", characters[i]);
-//         newButton.text(characters[i]);
-//         $("#button-view").append(newButton);
-//     }
-// }
-
-// renderButtons();
-
-// $("button").on("click", function () {
-//     event.preventDefault();
-//     var character = $("#gif-input").val().trim();
-//     characters.push(character);
-//     renderButtons();
-
-//     var character = $(this).attr("data-character");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
-
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     })
-//         .then(function (response) {
-//             var results = response.data;
-
-//             for (var i = 0; i < results.length; i++) {
-//                 var gifDiv = $("<div>");
-//                 var rating = results[i].rating;
-
-//                 var p = $("<p>").text("Rating: " + rating);
-
-//                 var characterImage = $("<img>");
-
-//                 characterImage.attr("src", results[i].images.fixed_height.url);
-//                 // still
-//                 // animate
-//                 // state
-//                 // div
-
-//                 gifDiv.prepend(p);
-//                 gifDiv.prepend(characterImage);
-
-//                 $("#gif").prepend(gifDiv);
-//             }
-
-//         })
-
-//         $(document).on("click", ".gif-btn", displayGif);
-
-
-//         function displayGif() {
-//             var gifDiv = $("<div>");
-//             $("#gif-view").prepend(gifDiv);
-//         }
-
-//         renderButtons();
-// })
-
-
-// $("#add-gif").on("click", function (event) {
-//     event.preventDefault();
-//     var character = $("#gif-input").val().trim();
-
-//     characters.push(character);
-//     renderButtons();
-
-//     // gifDiv.prepend(p);
-//     // $("#gif").prepend(gifDiv);
-// });
-
-
-
-// // $(".gif").on("click", function () {
-// //     var state = $(this).attr("data-state");
-// //     if (state === 'still') {
-// //         var animateURL = $(this).attr(queryURL);
-// //         $(this).attr("src", animateURL);
-// //         $(this).attr("data-state", "animate");
-// //     } else {
-// //         $(this).attr("src", $(this).attr("data-still"));
-// //         $(this).attr("data-state", "still");
-
-// //     };
-
-
-// // })
