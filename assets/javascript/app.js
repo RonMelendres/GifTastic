5118d009f0c12126b75efc4fa26fa8ced832b50a
Ron Melendres <Ron.melendres25@gmail.com>
2019-09-12 17:45:16 -0700

    fixed some funtion placements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 // var characters = ["mario", "luigi", "link"];
 
 // function displayGif() {
+
 //     var character = $(this).attr("data-character");
 
 //     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
@@ -22,7 +23,7 @@
 
 //             gifDiv.append(p);
 
-//             // var gifImage = $("<img");
+//             // var gifImage = $("<img>");
 
 //             // gifImage.attr("src", results[i].images.fixed_height.url);
 
@@ -31,22 +32,22 @@
 
 //             $("#gif-view").prepend(gifDiv);
 
-//             // for (var i = 0; i < results.length; i++) {
-//             //     var gifDiv = $("<div>");
-//             //     var rating = results[i].rating;
+//             for (var i = 0; i < results.length; i++) {
+//                 var gifDiv = $("<div>");
+//                 var rating = results[i].rating;
 
-//             //     var p = $("<p>").text("Rating: " + rating);
+//                 var p = $("<p>").text("Rating: " + rating);
 
-//             //     var characterImage = $("<img>");
-//             //     characterImage.attr("src", results[i].images.fixed_height.url);
+//                 var characterImage = $("<img>");
+//                 characterImage.attr("src", results[i].images.fixed_height.url);
 
-//             //     gifDiv.prepend(p);
-//             //     gifDiv.prepend(characterImage);
+//                 gifDiv.prepend(p);
+//                 gifDiv.prepend(characterImage);
 
-//             //     $("#gif-view").prepend(gifDiv);
-//             // }
+//                 $("#gif-view").prepend(gifDiv);
 
-//         })
+
+
 //     function renderButtons() {
 //         $("#button-view").empty();
 //         for (var i = 0; i < characters.length; i++) {
@@ -63,7 +64,7 @@
 //         var character = $("#gif-input").val().trim();
 //         characters.push(character);
 //         renderButtons();
-        
+
 //     });
 
 //     $(document).on("click", ".gif-btn", displayGif);
@@ -72,22 +73,37 @@
 
 
 
-var characters = [];
-function displayGif() {
-    var gifDiv = $("<div>");
-    $("#gif-view").prepend(gifDiv);
-}
+
+
+var characters = ["mario", "link", "yoshi"];
+
 
 // this is the main section that pulls gifs from giphy and from the button div in html
 // function displayGif() {
+
+function renderButtons() {
+    // renderButtons.empty();
+    $("#button-view").empty();
+    for (var i = 0; i < characters.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("gif-btn");
+        newButton.attr("data-name", characters[i]);
+        newButton.text(characters[i]);
+        $("#button-view").append(newButton);
+    }
+}
+
+renderButtons();
+
 $("button").on("click", function () {
-    // event.preventDefault();
-    // var character = $("#gif-input").val().trim();
-    // characters.push(character);
+    event.preventDefault();
+    var character = $("#gif-input").val().trim();
+    characters.push(character);
     // renderButtons();
 
     var character = $(this).attr("data-character");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8sdY7wYFOtrrcOTj2xGSGjj8SIqMafbF&q=" + character + "&limit=10&offset=0&rating=PG&lang=en";
+
 
     $.ajax({
         url: queryURL,
@@ -103,7 +119,12 @@ $("button").on("click", function () {
                 var p = $("<p>").text("Rating: " + rating);
 
                 var characterImage = $("<img>");
+
                 characterImage.attr("src", results[i].images.fixed_height.url);
+                // still
+                // animate
+                // state
+                // div
 
                 gifDiv.prepend(p);
                 gifDiv.prepend(characterImage);
@@ -113,45 +134,47 @@ $("button").on("click", function () {
 
         })
 
+        $(document).on("click", ".gif-btn", displayGif);
 
 
-    $(document).on("click", ".gif-btn", displayGif);
-    // renderButtons();
+        function displayGif() {
+            var gifDiv = $("<div>");
+            $("#gif-view").prepend(gifDiv);
+        }
+
+        renderButtons();
+})
 
 
-//     $(".gif").on("click", function () {
-//         var state = $(this).attr("data-state");
-//         if (state === 'still') {
-//             var animateURL = $(this).attr(queryURL);
-//             $(this).attr("src", animateURL);
-//             $(this).attr("data-state", "animate");
-//         } else {
-//             $(this).attr("src", $(this).attr("data-still"));
-//             $(this).attr("data-state", "still");
 
-//         };
 
-//         function renderButtons() {
-//             $("#button-view").empty();
-//             for (var i = 0; i < characters.length; i++) {
-//                 var newButton = $("<button>");
-//                 newButton.addClass("gif-btn");
-//                 newButton.attr("data-name", characters[i]);
-//                 newButton.text(characters[i]);
-//                 $("#button-view").append(newButton);
-//             }
-//         }
 
-//         $("#add-gif").on("click", function (event) {
-//             event.preventDefault();
-//             var character = $("#gif-input").val().trim();
 
-//             characters.push(character);
-//             // renderButtons();
 
-//             // gifDiv.prepend(p);
-//             // $("#gif").prepend(gifDiv);
-//         });
-//         renderButtons();
-//     })
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    var character = $("#gif-input").val().trim();
+
+    characters.push(character);
+    renderButtons();
+
+    gifDiv.prepend(p);
+    $("#gif").prepend(gifDiv);
+});
+
+
+
+$(".gif").on("click", function () {
+    var state = $(this).attr("data-state");
+    if (state === 'still') {
+        var animateURL = $(this).attr(queryURL);
+        $(this).attr("src", animateURL);
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+
+    };
+
+
 })
